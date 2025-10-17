21f868cfa178968dd7d535b111950c34a2f6b769
Irwin Eguiluz <ijea92@gmail.com>
2025-10-17 14:01:12 -0500

    import Input component and handle its onChange state value event

--- simple-form/src/components/UserSignupForm/UserSignupForm.jsx
+++ simple-form/src/components/UserSignupForm/UserSignupForm.jsx
@@ -1,14 +1,48 @@
+import { useState } from 'react';
+import Input from '../Input/Input';
+
 function UserSignupForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    firstname: '',
+    lastname: '',
+  });
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log('hello');
+    console.log(formData);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" placeholder="Your email address" required />
-      <input type="text" placeholder="Your first name" name="user-firstname" />
-      <input type="text" placeholder="Your last name" name="user-lastname" />
+    <form method="POST" onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        placeholder="Your email address"
+        name="email"
+        required
+        onChange={(e) => handleInputChange(e, 'email')}
+      />
+      <Input
+        placeholder="Your first name"
+        name="firstname"
+        onChange={(e) => handleInputChange(e, 'firstName')}
+      />
+      <Input
+        placeholder="Your last name"
+        name="lastname"
+        onChange={(e) => handleInputChange(e, 'lastName')}
+      />
       <input type="submit" value="Sign up" />
     </form>
   );
